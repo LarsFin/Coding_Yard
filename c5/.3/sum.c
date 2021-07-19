1cfc319b3d24f0fc4ffec0f1f2957a50f3221c44
Lars C. Finlay <larsfinlay@googlemail.com>
2021-07-19 11:52:30 +0100

    chapter 5 part 3 speed comp

---
+++ c5/.3/sum.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <time.h>
+
+// experiment: two functions; suma and sumb
+// both functions calculate the sum of integers in an array and return it
+// suma uses standard array index referencing in its calculation.
+// sumb increments pointer to iterate over the items in the array
+// calculate speeds of suma and sumb and compare them
+// estimation: both functions should have pretty similar performances as indexing an array should be pointing to array position under the hood.
+// results;
+// suma(a):        3.765000
+// sumb(a):        3.688000
+// tested a few times, sumb is marginally faster
+
+#define SIZE 20
+
+#define TIME(relays, process) {\
+    clock_t start, end;\
+    double elapsed;\
+    start = clock();\
+    for (int i = 0; i < relays; i++)\
+        process;\
+    end = clock();\
+    elapsed = (double)(end - start) / CLOCKS_PER_SEC;\
+    printf(#process ":\t%f\n", elapsed);\
+}
+
+int suma(int[]);
+int sumb(int *);
+
+int main()
+{
+    int relays = 10e7;
+    int a[SIZE] = { 15, 15, 20, 45, 80, 90, 25, 15, 5, 20,
+                    30, 55, 10, 25, 65, 5, 75, 30, 20, 35 };
+
+    TIME(relays, suma(a))
+    TIME(relays, sumb(a))
+
+    return 0;
+}
+
+int suma(int a[])
+{
+    int sum = 0;
+
+    for (int i = 0; i < SIZE; i++)
+        sum += a[i];
+
+    return sum;
+}
+
+int sumb(int *a)
+{
+    int sum, i;
+
+    for (sum = i = 0; i < SIZE; i++, a++)
+        sum += *a;
+
+    return sum;
+}
